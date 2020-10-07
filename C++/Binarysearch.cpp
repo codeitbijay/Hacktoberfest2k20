ea1d0727ca7ddd24ccedf0c64678490aeef8e382
reorezz <46937649+reorezz@users.noreply.github.com>
2020-10-07 03:00:02 -0700

    Create Binarysearch.cpp
    Binary search

---
+++ C++/Binarysearch.cpp
@@ -0,0 +1,36 @@
+// Binary Search 
+#include <iostream> 
+using namespace std; 
+  
+
+int binarySearch(int arr[], int l, int r, int x) 
+{ 
+    if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+  
+      
+        if (arr[mid] == x) 
+            return mid; 
+  
+      
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid - 1, x); 
+  
+        
+        return binarySearch(arr, mid + 1, r, x); 
+    } 
+  
+    
+    return -1; 
+} 
+  
+int main(void) 
+{ 
+    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int x = 10; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int result = binarySearch(arr, 0, n - 1, x); 
+    (result == -1) ? cout << "not Present in the array
+                   : cout << "we got the Number " << result; 
+    return 0; 
+} 
