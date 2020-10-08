f06fb1c7291fe0faaa8aebe72209c6f0d08adba8
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-10-08 12:46:32 +0530

    Using js inbuilt reverse function
    It will save time, storage and it's very efficient than previous logic

--- javascript/reverse array/reverse.js
+++ javascript/reverse array/reverse.js
@@ -1,11 +1,3 @@
 let array = [1, 2, 3, 4, 5]
 
-console.log(reverse(array))
-
-function reverse(arr){
-    let reversed = []
-    for(let i = arr.length - 1; i >= 0; i--){
-        reversed.push(arr[i])
-    }
-    return reversed
-}
+console.log(array.reverse())
