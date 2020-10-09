f9363e17b4c1e67e120158b58e1dc6e72b18c42a
pawanverma-16 <72162565+pawanverma-16@users.noreply.github.com>
2020-10-09 23:48:59 +0530

    Update Bubble Sort.cpp

--- C++/Bubble Sort.cpp
+++ C++/Bubble Sort.cpp
@@ -5,14 +5,17 @@ int main ()
    int i, j,temp,pass=0;
    int a[10] = {10,2,0,14,43,25,18,1,5,45};
    cout <<"Input list ...\n";
-   for(i = 0; i<10; i++) {
+   for(i = 0; i<10; i++) 
+   {
       cout <<a[i]<<"\t";
    }
 cout<<endl;
-for(i = 0; i<10; i++) {
+for(i = 0; i<10; i++) 
+{
    for(j = i+1; j<10; j++)
    {
-      if(a[j] < a[i]) {
+      if(a[j] < a[i]) 
+      {
          temp = a[i];
          a[i] = a[j];
          a[j] = temp;
@@ -21,7 +24,8 @@ for(i = 0; i<10; i++) {
 pass++;
 }
 cout <<"Sorted Element List ...\n";
-for(i = 0; i<10; i++) {
+for(i = 0; i<10; i++)
+{
    cout <<a[i]<<"\t";
 }
 cout<<"\nNumber of passes taken to sort the list:"<<pass<<endl;
