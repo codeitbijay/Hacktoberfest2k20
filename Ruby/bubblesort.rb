573c5cc4545fb965f66c5702f079c22e827f273d
tbeede <14968227+tbeede@users.noreply.github.com>
2020-10-13 16:50:18 -0400

    Create bubblesort.rb

---
+++ Ruby/bubblesort.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def bubble_sort(array)
+  return array if array.size <= 1
+  swap = true
+    while swap
+      swap = false
+      (array.length - 1).times do |x|
+        if array[x] > array[x+1]
+          array[x], array[x+1] = array[x+1], array[x]
+          swap = true
+        end
+      end
+    end
+  array
+end
