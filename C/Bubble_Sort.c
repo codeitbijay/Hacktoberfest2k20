891d21ebdb873913fdc5c14301f2a2f4fae68508
bhavinisehgal18 <62087213+bhavinisehgal18@users.noreply.github.com>
2020-10-17 14:46:30 +0530

    Update Bubble_Sort.c

--- C/Bubble_Sort.c
+++ C/Bubble_Sort.c
@@ -1,62 +1,44 @@
 #include <stdio.h>
-
-// Function for bubble sort
-void Bubble_Sort(int array[], int size)
+void swap(int *xp, int *yp)
 {
-    int temp, flag;
-    for (int i = 0; i < size - 1; i++)
-    {
-        flag = 0;
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            // Do swapping
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                flag++;
-            }
-        }
-        if (!flag)
-            break;
-    }
-
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
-
-// Function to print elements of array
-void Print_Array(int array[], int size)
+void bubble(int arr[], int i,int n)
 {
-    for (int i = 0; i < size; i++)
-        printf("%d ", array[i]);
-
+      for(int j=0; j<n-i-1;j++)
+      {
+          if(arr[j]>arr[j+1])
+          {
+              swap(&arr[j],&arr[j+1]);
+          }
+      }
+}
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+        bubble(arr, i, n);
+}
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
     printf("\n");
 }
-
-// Driver Function
 int main()
 {
-    int n;
-    printf("Enter number of elements in your array: ");
-    scanf("%d", &n);
-    int array[n];
-    printf("Enter your array: ");
-    for (int i = 0; i < n; i++)
-        scanf("%d", &array[i]);
-    Bubble_Sort(array, n);
-    Print_Array(array, n);
-    return 0;
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--)
+    {
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;;
 }
-
-/* INPUT: 
-Enter number of elements in your array: 7
-Enter your array: 9
-8
-7
-2
-6
-3
-5
-OUTPUT:
-2 3 5 6 7 8 9 */
-
