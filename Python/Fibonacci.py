660e8415bbd7a6e1ee4ca9f4f622cd793329ea20
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-10-17 11:54:09 +0700

    Create Fibonacci.py

---
+++ Python/Fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+num = eval(input("Fibonacci Sequence, Enter a Number: "))
+
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence: ")
+   for i in range(num):
+       print(recur_fibo(i))
