58c19d21b011d5c6acdcff2d3c1430a66192f1ed
Fraud Vestige <73070611+fraudVestige@users.noreply.github.com>
2020-10-18 21:11:59 +0530

    3*3 maze solution
    3*3 maze solution

---
+++ C++/3x3 maze.cpp
@@ -0,0 +1,36 @@
+// Anant Raj
+// Indian Institute of Information Technology, Bhopal
+#include <bits/stdc++.h>
+#define lli long long int
+#define ull unsigned long long
+#define fl(i,a,b) for(i=a ; i<b ; i++)
+#define test lli t;cin>>t;while(t--)
+#define vec vector<lli>
+#define mp map<lli,lli>
+#define deq deque<lli>
+#define pb(a) push_back(a)
+#define all(x) (x.begin(),x.end())
+#define MOD 1000000007
+#define fast std::ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define r0 return 0
+using namespace std;
+int main()
+{
+    fast
+    test
+    {
+        lli n,i,val;
+        cin>>n;
+        set <lli> s;
+        fl (i,0,n)
+        {
+            cin>>val;
+            s.insert(val);
+        }
+        if (s.size()!=n)
+            cout<<"YES\n";
+        else
+
+    }
+    r0;
+}
