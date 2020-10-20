e3d432694b9741df9923ec86607cc2fe807cf457
David Aponte <alexdavidve@gmail.com>
2020-10-19 21:26:03 -0400

    Image Edit

--- Python/char_removal_from_string.py
+++
@@ -1,6 +0,0 @@
-a=input("enter string ")
-b=input("enter character ")
-for i in range (len(a)):
-    if b != a[i]:
-        print(a[i],end=" ")
-
--- javascript/dynamic programming/fibonacci.js
+++
@@ -1,9 +0,0 @@
-const nthFibonacci = (n) => {
-  // set the base case(fib[0] = 0, fib[1] = 1)
-  let fib = [0, 1]
-  for (let i = 2; i <= n; i++) {
-      fib.push(fib[i - 1] + fib[i - 2]);
-  }
-
-  return fib[n];
-}
