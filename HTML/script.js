c4ab4e8958f15386f42c97644e931f619655934a
Roshan Khausiya <34279341+cyberknight-np@users.noreply.github.com>
2020-10-21 18:53:31 +0545

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,5 @@
+// Loading animation with keyframe for SCSS & Jade
+
+// inspired by <http://www.grafikart.fr/tutoriels/html-css/loaders-animes-496>
+
+// Author Joël Lesenne <https://joellesenne.dev>
