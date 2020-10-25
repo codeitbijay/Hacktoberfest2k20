fe9282285d870d309ed199f8c146451272ae70ef
Vijay Chauhan <63604585+mr-vijaychauhan@users.noreply.github.com>
2020-10-25 20:58:08 +0530

    Create Quotes Auto Generator Apps

---
+++ javascript/quotesGeneratorApps/app.js
@@ -0,0 +1,18 @@
+const requestURL = "https://type.fit/api/quotes";
+
+async function getQuotes(name) 
+{
+  let response = await fetch(requestURL);
+  let data = await response.json();
+  return data;
+}
+
+getQuotes().then(data => {
+    let rand_num=Math.floor(Math.random() * 1500);
+    let quotes_detail=data[rand_num];
+    console.log(quotes_detail);
+    let text=quotes_detail['text'];
+    let author=quotes_detail['author'];
+    $("h2").append(text);
+    $("p").append(author);
+}); 
