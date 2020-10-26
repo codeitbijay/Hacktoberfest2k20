3ce201dfa566b19543e86a5ccad9bfed2d448dc2
AKHIL-S-BABU <53579216+AKHIL-S-BABU@users.noreply.github.com>
2020-10-26 18:11:25 +0530

    Python Program to Calculate length of a String
    Or you can just use the library function len().

---
+++ String_length.py
@@ -0,0 +1,8 @@
+# User inputs the string and it gets stored in variable str
+str = input("Enter a string: ")
+
+# c variable to count the character in a string
+c = 0
+for s in str:
+      c = c+1
+print("Length of the input string is:", c)
