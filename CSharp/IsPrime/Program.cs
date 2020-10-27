c5724556645d8c8f19cf6750b6d7088a734f97d1
anilswm <anilswm84@gmail.com>
2020-10-27 13:53:26 +0530

    Added a IsPrime Alogrithm in C Sharp

---
+++ CSharp/IsPrime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            var input=Console.ReadLine();
+            var number = Convert.ToInt64(input);
+            var isPrime = IsPrime(number);
+            Console.WriteLine(isPrime ? $"{number} is a Prime number." : $"{number} is not a Prime number.");
+            Console.ReadLine();
+        }
+
+        public static bool IsPrime(long num)
+        {
+            if (num <= 1) return false;
+            if (num == 2) return true;
+            if (num % 2 == 0) return false;
+
+            var mid = (int)Math.Floor((Math.Sqrt(num)));
+
+            for (int i = 3; i <= mid; i += 2)
+            {
+                if ((num % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
