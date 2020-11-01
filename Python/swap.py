9034c56c438cb9fcc57fbd2c678b6b35f2b0f18c
Zeeshan Ali <zeeshan.ali0215@gmail.com>
2020-10-31 20:30:00 -0400

    Added swap.py which takes input and swaps

---
+++ Python/swap.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#Swapping two numbers by taking input from the user
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+print("Before swap a= "+a+"  &  b= "+b)
+a,b=b,a
+print("After swap a= "+a+"  &  b= "+b)
