ec23144efdc0787f650d17fc20e3ca343ee85a12
hv4114 <92599851+hv4114@users.noreply.github.com>
2021-10-16 22:15:59 +0530

    Reversestring_faster&shorter_script_byHarsh.py
    I updated and contracted the script to three LOC and it's faster than previous one. It functions faster too. 
    Please provide approval.
    Thanks

--- Python/reverseString.py
+++ Python/reverseString.py
@@ -1,16 +1,3 @@
-# Python code to reverse a string 
-# using recursion 
-
-def reverse(s): 
-	if len(s) == 0: 
-		return s 
-	else: 
-		return reverse(s[1:]) + s[0] 
-
-s = "Geeksforgeeks"
-
-print ("The original string is : ",end="") 
-print (s) 
-
-print ("The reversed string(using recursion) is : ",end="") 
-print (reverse(s)) 
+s = input()
+d = s[::-1]
+print (d)
