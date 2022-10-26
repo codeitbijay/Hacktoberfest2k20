bf31ce862e624ae582e6d45a222c95cc5b7b0109
Shreya <91543981+shreya71@users.noreply.github.com>
2022-10-26 11:40:33 +0530

    Update and rename Wiggle Sequence.cpp to Wiggle Subsequence.cpp

--- C++/Wiggle Subsequence.cpp
+++ C++/Wiggle Subsequence.cpp
@@ -1,5 +1,5 @@
 /*
-A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
+A wiggle subsequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
 For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) alternate between positive and negative.
 In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences. The first is not because its first two differences are positive, and the second is not because its last difference is zero.
 A subsequence is obtained by deleting some elements (possibly zero) from the original sequence, leaving the remaining elements in their original order.
